20a28c9e62b8dd0e6490f65829f9b5e6f052d63d
Javad <javad@K2-desktop.(none)>
2013-01-14 11:38:13 -0500

    fibers intro

--- elliptic.py
+++ elliptic.py
@@ -6,7 +6,54 @@ from sage.rings.arith import CRT, factor
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.schemes.elliptic_curves.constructor import EllipticCurve_from_j
 from itertools import izip_longest
-from de_compose import *
+
+def _compose(Ps, f, g):
+    if len(Ps) == 0:
+        return f.parent()(0)
+    elif len(Ps) == 1:
+        return f.parent()(Ps[0])
+    else:
+        f2, g2 = f**2, g**2
+        P0 = _compose(Ps[::2], f2, g2)
+        P1 = _compose(Ps[1::2], f2, g2)
+        if len(Ps) % 2 == 0:
+            return g*P0 + f*P1
+        else:
+            return P0 + g*f*P1
+
+
+def _decompose(P, f, g, target):
+    "Assumes deg f >= deg g"
+
+    def dec(P, f, g, invf, invg):
+        a = f.parent()(0)
+        if P.degree() >= f.degree() + g.degree():
+            a, P = P.quo_rem(f)
+            
+        return a + (P * invf) % g, (P * invg) % f
+        
+    n, a, b = 0, 1, 1
+    moduli = []
+    for bit in reversed(target.bits()):
+        if bit:
+            n1, a1, b1 = n+1, a*f, b*g
+        else:
+            n1, a1, b1 = n, a, b
+        moduli.append((n1, a1, b1))
+        n, a, b = n + n1, a*a1, b*b1
+
+    res = {0: P}
+    for n, a, b in reversed(moduli):
+        newres = {}
+        _, inva, invb = a.xgcd(b)
+        for i, p in res.iteritems():
+            p0, p1 = dec(p, a, b, inva, invb)
+            newres[i+n] = p0 + newres.get(i+n, 0)
+            newres[i] = p1 + newres.get(i, 0)
+        res = newres
+
+    return [res[i] for i in range(len(res))]
+
 
 class Tower:
     def __init__(self, K, ell, name, debug=False):
@@ -83,8 +130,8 @@ class Tower:
         F, G = (self._abs_polys[-1] 
                 if self._abs_polys
                 else (f.parent().gen(), f.parent()(1)))
-        F = compose(list(F), f, g)
-        G = g*compose(list(G), f, g)
+        F = _compose(list(F), f, g)
+        G = g*_compose(list(G), f, g)
         self._abs_polys.append((F, G))
         p = F - self._eta*G
 
@@ -95,7 +142,7 @@ class Tower:
                check_irreducible=self._debug)
         self._levels.append(K)
 
-    def _push(self, x):
+    def _push_down(self, x):
         level = x.parent().degree().valuation(self._degree)
         f, g = self._rel_polys[-level % len(self._rel_polys)]
         deg = self._degree**(level - 1) - 1
@@ -103,12 +150,12 @@ class Tower:
         P = f.parent(x.polynomial())
 
         return [self[level-1](list(c))
-                for c in izip_longest(*decompose(P, f, g, deg))]
+                for c in izip_longest(*_decompose(P, f, g, deg))]
 
-    def _lift(self, xs):
+    def _lift_up(self, xs):
         level = xs[0].parent().degree().valuation(self._degree)
         f, g = self._rel_polys[(-level-1) % len(self._rel_polys)]
-        Ps = map(f.parent().__call__, izip_longest(*map(lambda x:x.polynomial(), xs)))
+        Ps = list(izip_longest(*map(lambda x:x.polynomial(), xs)))
 
-        return (self[level+1](compose(Ps, f, g)) / 
+        return (self[level+1](_compose(Ps, f, g)) / 
                 self[level+1](g**(len(Ps)-1)))
--- two_torus.py
+++ two_torus.py
@@ -6,7 +6,6 @@ from sage.rings.arith import CRT, factor
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.misc.functional import cyclotomic_polynomial
 from itertools import izip_longest
-from de_compose import *
 
 def _torsion_poly(ell, mod=None):
     """
@@ -45,40 +44,18 @@ def _torsion_poly(ell, mod=None):
     return P(list(reversed(t))).shift(ell % 2 - 1)
 
 
-# Montgomery ladder for Pell conics
-def _pellmul(x, n):
-    # The zero point and ours
-    A, B = 2, x
-    for c in reversed(n.digits(2)):
-        if c == 0:
-            A, B = A**2 - 2, A*B - x
-        else:
-            A, B = A*B - x, B**2 - 2
-    return A
-
-
 class Tower:
     def __init__(self, K, ell, name, debug=False):
         """
         Initialize the ell-adic extension tower of K.
         """
-        p = K.characteristic()
-
-        if not K.is_finite():
-            raise RuntimeError('The field must be finite.')
-        if  not K.is_prime_field():
-            raise NotImplementedError('Only works for prime fields.')
-        if (p + 1) % ell != 0:
-            raise RuntimeError('The degree must divide %d.' % (p + 1))
+        if not K.is_finite() and not K.is_prime_field():
+            raise RuntimeError('Only works for prime fields.')
+        if (K.characteristic() + 1) % ell != 0:
+            raise RuntimeError('The degree must divide ' +
+                               str(K.characteristic() + 1) + '.')
         if ell % 2 == 0:
             raise RuntimeError('The degree must be odd.')
-
-        # Find an element of maximal order on the Pell conic
-        eta = K(1)
-        if p != 2:
-            o = (p + 1) // ell
-            while (eta**2 - 4).is_square() or _pellmul(eta, o) == 2:
-                eta = K.random_element()
 
         self._base = K
         self._degree = ell
@@ -86,7 +63,7 @@ class Tower:
         self._t = _torsion_poly(ell, K.characteristic())
         self._levels = [K]
         self._minpolys = [None]
-        self._eta = eta
+        self._eta = cyclotomic_polynomial(ell).factor_mod(K.characteristic())[0][0][1]
         self._debug = debug
 
     def __getitem__(self, i):
@@ -109,17 +86,32 @@ class Tower:
                check_irreducible=self._debug)
         self._levels.append(K)
 
-    def _push(self, x):
+    def _push_down(self, x):
         f = self._t
         P = f.parent(x.polynomial())
         level = x.parent().degree().valuation(self._degree)
 
-        return [self[level-1](list(c))
+        def decompose(P, f):
+            if P.degree() < f.degree() * 2:
+                return P.quo_rem(f)
+            else:
+                return sum((g.quo_rem(f) for g in decompose(P, f**2)), ())
+
+        return [self[level-1](list(reversed(c)))
                 for c in izip_longest(*decompose(P, f))]
 
-    def _lift(self, xs):
+    def _lift_up(self, xs):
         f = self._t
-        Ps = map(f.parent().__call__, izip_longest(*map(lambda x:x.polynomial(), xs)))
+        Ps = list(izip_longest(*map(lambda x:x.polynomial(), xs)))
         level = xs[0].parent().degree().valuation(self._degree)
+
+        def compose(Ps, f):
+            if len(Ps) == 0:
+                return f.parent()(0)
+            elif len(Ps) == 1:
+                return f.parent()(Ps[0])
+            else:
+                g = f**2
+                return compose(Ps[::2], g) + f*compose(Ps[1::2], g)
 
         return self[level+1](compose(Ps, f))
